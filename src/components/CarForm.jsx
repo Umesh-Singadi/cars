95ce1fc46c89deea832c72e20688738d138c20f5
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-03 15:13:18 +0530

    Integrate Redux for state management in CarForm componenet

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,5 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import { changeName } from "../store/slices/formSlice";
 function CarForm() {
-  return <div>CarForm</div>;
+  const { name } = useSelector((state) => state.form);
+  const dispatch = useDispatch();
+
+  function handleNameChange(event) {
+    dispatch(changeName(event.target.value));
+  }
+
+  const handleSubmit = (e) => e.preventDefault();
+
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form className="field-group" onSubmit={handleSubmit}>
+        <div className="field">
+          <label>Name: </label>
+          <input
+            type="text"
+            className="input is-expanded"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
