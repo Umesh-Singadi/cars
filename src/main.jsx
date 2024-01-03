81a275baaaa90ddcfa3cdaaea92aed19a6b2a4fd
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-03 16:21:42 +0530

    Add bulma CSS

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/index.jsx";
-
+import "./styles.css";
+import "bulma/css/bulma.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
